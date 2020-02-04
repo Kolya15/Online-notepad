fad63ae3d67290a2dcf39f6bd373eeb8121c1aec
Kolya15 <kolya.soroka15@gmail.com>
2020-02-04 21:32:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 // Проверка существования localStorage и вставка содержимого в форму
-let textNote = document.querySelector('.note');
-let dateEditing = document.querySelector('.date-editing');
-if (localStorage){
-    let note1 = JSON.parse(localStorage.getItem ("note"));
-    console.log(note1);
-    textNote.value = note1.text;
-    dateEditing.innerHTML = note1.date;
+let textNote = document.querySelector(".note");
+let dateEditing = document.querySelector(".date-editing");
+if (localStorage.getItem("note") !== null) {
+  let note1 = JSON.parse(localStorage.getItem("note"));
+  console.log(note1);
+  textNote.value = note1.text;
+  dateEditing.innerHTML = note1.date;
 }
 
 let nowDate = new Date();
 
 let note = {
-    text: "",
+  text: ""
 };
 
-// let textNote = document.querySelector('.note');
-function saveNote(){
-    note.text = textNote.value;
-    note.date = nowDate.toLocaleString();
-    localStorage.setItem ("note", JSON.stringify(note));
-};
+// let textNote = document.querySelector(".note");
+function saveNote() {
+  note.text = textNote.value;
+  note.date = nowDate.toLocaleString();
+  console.log(note);
+  localStorage.setItem("note", JSON.stringify(note));
+}
 
 window.onunload = saveNote;
