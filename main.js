b68ab1df21628557e5ec36e66669e5ce98dcaa02
Nikolay-Front-end <kolya.soroka15@gmail.com>
2020-02-04 14:32:16 +0200

    Завершение логики

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
+// Проверка существования localStorage и вставка содержимого в форму
+let textNote = document.querySelector('.note');
+let dateEditing = document.querySelector('.date-editing');
+if (localStorage){
+    let note1 = JSON.parse(localStorage.getItem ("note"));
+    console.log(note1);
+    textNote.value = note1.text;
+    dateEditing.innerHTML = note1.date;
+}
+
 let nowDate = new Date();
 
 let note = {
-    text: "sdsd"
+    text: "",
 };
 
-let textNote = document.querySelector('.note');
-let note1;
+// let textNote = document.querySelector('.note');
 function saveNote(){
     note.text = textNote.value;
     note.date = nowDate.toLocaleString();
     localStorage.setItem ("note", JSON.stringify(note));
-    note1 = JSON.parse (localStorage.getItem ("note"));
-    console.log(note1);
 };
 
-
-window.onbeforeunload = function saveNote1(){
-
-};
-
+window.onunload = saveNote;
